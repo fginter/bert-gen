41819acc9158bd5da5499fe37effc864085c3f20
Filip Ginter <filip.ginter@gmail.com>
2019-10-22 14:18:29 +0300

    apex

--- ftune.py
+++ ftune.py
@@ -1,3 +1,4 @@
+import time
 import datetime
 import transformers
 import torch
@@ -17,16 +18,20 @@ def batches_from_filenames(fnames,tokenizer):
     for f_name in fnames:
         with open(f_name,"rt") as f:
             doc_examples=ds.doc_examples_from_plaintext(f)
-            batches=ds.batches_from_documents(doc_examples,tokenizer,max_length=60)
+            batches=ds.batches_from_documents(doc_examples,tokenizer,max_length=75)
             yield from batches
 
 if __name__=="__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser()
+    parser.add_argument("--apex",default=False,action="store_true",help="Half-prec training w/ apex")
     parser.add_argument("--files", nargs="+", help=".txt files used to train")
     parser.add_argument("--out", help="out model filename")
     parser.add_argument("--log", help="logfile")
     args = parser.parse_args()
+
+    if args.apex:
+        import apex
     
     model=transformers.BertForMaskedLM.from_pretrained("bert-base-finnish-cased")
     tokenizer = transformers.BertTokenizer.from_pretrained("bert-base-finnish-cased")
@@ -37,8 +42,15 @@ if __name__=="__main__":
         {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.0},
         {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.025}
     ]
+    t_total=5000000
     optimizer=transformers.optimization.AdamW(optimizer_grouped_parameters,lr=0.00001)
-    scheduler = transformers.WarmupLinearSchedule(optimizer, warmup_steps=100, t_total=1000000)
+    scheduler = transformers.WarmupLinearSchedule(optimizer, warmup_steps=10000, t_total=t_total)
+
+    if args.apex:
+        opt_level = 'O1'
+        model, optimizer = apex.amp.initialize(model, optimizer, opt_level=opt_level)
+
+
     model.zero_grad()
     model.train()
 
@@ -46,7 +58,7 @@ if __name__=="__main__":
     examples_seen=0
     batches_seen=0
     
-
+    time_started=time.time()
     with open(args.log,"wt") as logfile:
         for idx,x in enumerate(batches):
             inp,mask,outp=x
@@ -57,10 +69,17 @@ if __name__=="__main__":
             optimizer.zero_grad()
             outputs=model(inp,attention_mask=mask,masked_lm_labels=outp)
             loss=outputs[0]
-            loss.backward()
+            if args.apex:
+                with apex.amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+            else:
+                loss.backward()
             optimizer.step()
             scheduler.step()
             if idx and idx%10==0:
-                print(idx,loss.item(),datetime.datetime.now().isoformat(),examples_seen,sep="\t",file=logfile,flush=True)
+                elapsed=time.time()-time_started
+                print(idx,loss.item(),datetime.datetime.now().isoformat(),examples_seen,idx/elapsed,"[batch/sec]",examples_seen/elapsed,"[ex/sec]",sep="\t",file=logfile,flush=True)
             if idx and idx%10000==0:
                 model.save_pretrained(args.out)
+            if idx>=t_total:
+                break
